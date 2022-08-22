8502b3bb70a9d146c0380ed45200cbbf304d800e
Asser Elfeki <66274223+AsserElfeki@users.noreply.github.com>
2022-08-22 12:45:00 +0200

    more organization (there is a bug in swiper)

--- gallery.js
+++ gallery.js
@@ -1,18 +1,21 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    centeredSlides: true,
-    loop: false,
-    effect: "coverflow",
-    grabCUrsor: true,
+    // centeredSlides: true,
+    // loop: false,
+    // effect: "coverflow",
+    // grabCUrsor: true,
+    // speed: 800,
+    // spaceBetween: 0,
+
     pagination: {
         el: '.swiper-pagination',
       },
     
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     
   });
